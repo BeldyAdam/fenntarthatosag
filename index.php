a55e059c9c08ccbf2e7d15da6882ea7a69e00377
BeldyAdam <beldy.adam@gmail.com>
2023-03-22 12:22:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Béldy Ádám</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <div>
+            <img src="kepek/Képernyőkép 2023-03-22 114626.png" alt="ps">
+
+        </div>
+        <div>
+            <?php
+            $elsoSzint = "Ökoszisztéma";
+            $masodikSzint = "Társadalom";
+            $harmadikSzint = "Gazdaság";
+            $negyedikSzint = "Pénz";
+            echo "<ul>";
+            echo "<li>$elsoSzint</li>";
+            echo "<li>$masodikSzint</li>";
+            echo "<li>$harmadikSzint</li>";
+            echo "<li>$negyedikSzint</li>";
+            echo "</ul>";
+
+            ?>
+        </div>
+        <div>
+            <?php
+            echo "<tr>";
+            echo "  <th>A megújuló erőforrások fogyasztása</th>
+                    <th>A környezet helyzete</th>
+                    <th>Fenntarthatóság</th>";
+            echo "</tr>";
+            echo "<br>";
+            echo "<tr>";
+            echo "  <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>";
+            echo "</tr>";
+            echo "<br>";
+            echo "<tr>";
+            echo "  <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>";
+            echo "</tr>";
+            echo "<br>";
+            echo "<tr>";
+            echo "  <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>";
+            echo "</tr>";
+            ?>
+        </div>
+    </main>
+    <!-- befejezni hetfo 10ig -->
+
+</body>
+
+</html>
